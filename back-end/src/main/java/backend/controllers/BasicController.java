5cf7f4ed39a4b2f09b48092ba88a51c7c97c1466
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-04-26 16:07:33 +0100

    java was a mistake btw

--- back-end/src/main/java/backend/controllers/BasicController.java
+++ back-end/src/main/java/backend/controllers/BasicController.java
@@ -1,13 +1,17 @@
 package backend.controllers;
 
+import backend.dao.Users;
+import backend.dao.UsersDAO;
+import backend.json.UserJSON;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BasicController {
 
-    @GetMapping("/hello-world")
-    public String helloWorld() {
-        return "Hello World!";
+    @GetMapping("/test")
+    public UserJSON helloWorld() throws Exception{
+        return new UserJSON(UsersDAO.getUsersByORMID(1));
     }
 
 }
