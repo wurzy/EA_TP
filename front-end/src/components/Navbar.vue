e03a173f759462e6d9eb672c8afd6d7154a100b7
fliper6 <filipa.alvesantos@outlook.com>
2021-06-10 20:44:57 +0100

    navbar e app.vue set

---
+++ front-end/src/components/Navbar.vue
@@ -0,0 +1,106 @@
+<template>
+  
+  <div id="app">
+  <v-app id="inspire">
+    <v-layout row justify-center>
+      <v-toolbar app color="#57a2bf" class="hidden-xs-and-down">
+        <v-img
+        class="logo"
+        lazy-src="../assets/redu.png"
+        max-height="40"
+        max-width="40"
+        src="../assets/redu.png"
+        ></v-img>
+
+        <v-toolbar-items>
+         <v-btn class="navbar-button">
+            <router-link class="hyperlink" to="/">Home</router-link>
+         </v-btn>
+         <v-btn class="navbar-button">
+            <router-link class="hyperlink" to="/recursos">Recursos</router-link>
+         </v-btn>
+         <v-btn class="navbar-button">
+            <router-link class="hyperlink" to="/">Utilizadores</router-link>
+         </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+         <v-btn class="navbar-button">
+            <router-link class="hyperlink" to="/">Perfil</router-link>
+         </v-btn>
+         <v-btn class="navbar-button">
+            <router-link class="hyperlink" to="/">Logout</router-link>
+         </v-btn>
+        </v-toolbar-items>
+        </v-toolbar>
+    </v-layout>
+  </v-app>
+  </div>
+
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data () {
+    return {
+      dialog: false,
+      nav: [
+        {
+          icon: 'home',
+          text: 'Home',
+          link: '../views/Recurso.vue',
+          title: '../views/Recurso.vue',
+          active: true
+        },
+        {
+          icon: 'recursos',
+          text: 'Recursos',
+          //title: '',
+          active: false
+        },
+        {
+          icon: 'utilizadores',
+          text: 'Utilizadores',
+          //title: '',
+          active: false
+        },
+        {
+          icon: 'perfil',
+          text: 'Perfil',
+          //title: '',
+          active: false
+        },
+        {
+          icon: 'logout',
+          text: 'Logout',
+          //title: '',
+          active: false
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+.navbar-button {
+    background-color: #57a2bf !important; 
+    padding-top: 10px !important; 
+    box-shadow: 0px 0px 0px 0px rgb(0 0 0 / 20%) !important;
+}
+
+.hyperlink {
+    text-decoration: none;
+    color: #212121 !important;
+}
+
+.logo{
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+}
+</style>
+
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import vuetify from "./plugins/vuetify";
+import "vuetify/dist/vuetify.min.css";
+
 import router from './router'
 
 Vue.config.productionTip = false;
